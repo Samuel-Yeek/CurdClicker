ab55482c7c1e2da4379de229626ccbf642393603
Samuel-Yeek <syeek@my.wctc.edu>
2024-02-19 01:54:47 -0600

    adding assets

--- upgrades.py
+++
@@ -1,7 +0,0 @@
-class Upgrade:
-    def __init__(self, name, cost, curds_per_second):
-        self.name = name
-        self.cost = cost
-        self.curds_per_second = curds_per_second
-
-        
